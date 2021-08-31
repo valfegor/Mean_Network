4d7412bb873ae4999586a4039c70a751ed5e41e0
Anthony Ahumada <locopala99@gmail.com>
2021-08-31 09:50:33 +0200

    more js

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './home/login/login.component';
+import { RegisterComponent } from './home/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:LoginComponent,
+    pathMatch:'full'
+    
+  },
+  {
+    path:'registerUser',
+    component:RegisterComponent,
+    pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -12,6 +12,20 @@ import { RegisterComponent } from './home/register/register.component';
 import { PostComponent } from './user/post/post.component';
 import { ListPostComponent } from './user/list-post/list-post.component';
 
+import { FormsModule,ReactiveFormsModule } from "@angular/forms";
+
+//import angular material.
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from "@angular/material/icon";
+import { MatDatepickerModule} from "@angular/material/datepicker";
+import {MatGridListModule} from '@angular/material/grid-list'; 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,7 +40,18 @@ import { ListPostComponent } from './user/list-post/list-post.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule, 
+    MatToolbarModule, 
+    MatButtonModule,
+    MatFormFieldModule, 
+    MatCardModule,
+    MatIconModule, 
+    MatInputModule,   
+    MatSnackBarModule,
+    MatDatepickerModule,
+    MatGridListModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
