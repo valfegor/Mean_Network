bad85c6ddf0bcf1c79cfdfe0de8810cfdd1a1803
Anthony Ahumada <locopala99@gmail.com>
2021-08-31 13:25:23 +0200

    more js

--- Frontend/src/app/post/register-post/register-post.component.ts
+++ Frontend/src/app/post/register-post/register-post.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from "../../services/post.service";
+import { Router } from "@angular/router";
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-register-post',
@@ -6,10 +13,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-post.component.css']
 })
 export class RegisterPostComponent implements OnInit {
-
-  constructor() { }
+  public registerData: any;
+  public message: string;
+  public horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  public VerticalPosition: MatSnackBarVerticalPosition = 'top';
+  public duratioInseconds: number;
+  constructor(private _postService:PostService,private _router:Router  , private _snackbar:MatSnackBar ) {
+    this.message = '';
+    this.duratioInseconds=2;
+    this.registerData={};
+   }
 
   ngOnInit(): void {
+  }
+
+  registerPost(){
+    if(!this.registerData.text || !this.registerData.estade){
+      this.message = 'Sorry Check all the camps',
+      this.openSnackBarError;
+      this.registerData={};
+    }
+    else{
+      this._postService.registerPost(this.registerData).subscribe(
+        (res)=>{
+          this._router.navigate(['/registerUser']);
+          this.message='Success Register the post';
+          this.openSnackBarError();
+          this.registerData={};
+        }, 
+        (err)=>{
+          this.message = err.error;
+          this.openSnackBarError();
+        }
+      )
+    }
+  }
+
+
+
+  openSnackBarSuccesfull() {
+    //this.messague = por que ha estado cambiando , {} = CONFIGURACIONES DE LA BARRA , propiedad de la duracion 
+    this._snackbar.open(this.message,'X',{
+      horizontalPosition:this.horizontalPosition,
+      verticalPosition:this.VerticalPosition,
+      duration:this.duratioInseconds*1000,
+      panelClass:['style-snackBarTrue']
+    });
+  }
+
+  openSnackBarError() {
+    this._snackbar.open(this.message,'X',{
+      horizontalPosition:this.horizontalPosition,
+      verticalPosition:this.VerticalPosition,
+      duration:this.duratioInseconds*1000,
+      panelClass:['style-snackBarFalse']
+    });
   }
 
 }
---
+++ Frontend/src/app/services/post.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Router } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  env:string;
+  constructor(private _router:Router , private _http:HttpClient) {
+    this.env = environment.APP_URL;
+   }
+
+   registerPost(post:any){
+    return this._http.post<any>(this.env+'post/registerPost',post);
+   }
+}
