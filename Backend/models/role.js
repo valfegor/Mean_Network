48765805a9bc99b783b6e5d5f66bf68b74038339
Anthony Ahumada <locopala99@gmail.com>
2021-08-31 08:26:29 +0200

    more js

---
+++ Backend/models/role.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const roleSchema = new mongoose.Schema({
+    name:String,
+    description:String,
+    date: { type: Date, default: Date.now },
+    Status: Boolean,
+});
+
+
+const role = mongoose.model('role',roleSchema);
+
+module.exports = role;
